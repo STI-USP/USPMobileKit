02e3aa77dbc4d6be2701f5e822ce26c1be9b6f30
Vagner Machado <82machado@gmail.com>
2025-09-04 17:08:19 -0300

    Registra token baseado no environment

--- Sources/USPAuthKit/Core/OAuthConfig.h
+++ Sources/USPAuthKit/Core/OAuthConfig.h
@@ -4,16 +4,3 @@
 //
 //  Created by Vagner Machado on 22/05/25.
 //
-
-#import <Foundation/Foundation.h>
-
-NS_ASSUME_NONNULL_BEGIN
-
-/// URL base única para OAuth
-//#if DEV
-static NSString * const kOAuthServiceBaseURL = @"https://dev.uspdigital.usp.br/mobile/servicos/oauth";
-//#else
-//static NSString * const kOAuthServiceBaseURL = @"https://uspdigital.usp.br/mobile/servicos/oauth";
-//#endif
-
-NS_ASSUME_NONNULL_END
